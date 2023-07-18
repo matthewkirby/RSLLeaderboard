f142e2978247f2c0266b6db4676cf37cbbd9f5d2
Xopar <xoparr@gmail.com>
2023-07-17 23:36:54 -0500

    Small reorg to main func

--- scripts/generate_leaderboard.py
+++ scripts/generate_leaderboard.py
@@ -4,11 +4,7 @@ import calculate_ratings as cr
 import settings
 
 
-def main(reset):
-    if reset:
-        dba.delete_databases()
-        return
-
+def main():
     dba.initialize_databases()
     dba.load_json_races()
     cr.calculate_ratings(settings.current_season)
@@ -17,6 +13,9 @@ def main(reset):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate Leaderboard')
     parser.add_argument('--reset', action='store_true', help='Reset the database')
-
     args = parser.parse_args()
-    main(args.reset)
+
+    if args.reset:
+        dba.delete_databases()
+    else:
+        main()
