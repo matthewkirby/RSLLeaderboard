d6e4a9cdf014e39fc0b3919bf834094a3ca85397
Xopar <xoparr@gmail.com>
2023-08-01 18:33:42 -0500

    Moved Loading.svg code to the module its used in to speed up loading it in and reduce the files needed to be served

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -6,7 +6,13 @@ const Loading: React.FC = () => {
   return (
     <div className={styles.container}>
       <span className={styles.text}>Loading...</span>
-      <img src="/images/Loading.svg" className={styles.graphic} alt="Loading..." />
+      <svg className={styles.graphic} version="1.1" viewBox="0 0 157.47 136.59" xmlns="http://www.w3.org/2000/svg">
+        <g fill="#d7d000" fillRule="evenodd">
+          <path transform="matrix(1.4137 0 0 1.4131 -34.298 61.671)" d="m79.893 53.017-55.632-1e-6 27.816-48.179z" stopColor="#000000"/>
+          <path transform="matrix(1.4137 0 0 1.4131 5.213 -6.8373)" d="m79.893 53.017-55.632-1e-6 27.816-48.179z" stopColor="#000000"/>
+          <path transform="matrix(1.4137 0 0 1.4131 44.519 61.242)" d="m79.893 53.017-55.632-1e-6 27.816-48.179z" stopColor="#000000"/>
+        </g>
+      </svg>
     </div>
   );
 };
