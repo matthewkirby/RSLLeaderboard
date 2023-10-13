f626fc55abc0493055bc1a18c12d1dfba8f71e1f
Xopar <xoparr@gmail.com>
2023-10-13 15:41:24 -0500

    This is part of the last commit, forgot to put it together. rip

--- shared/public_data_access.py
+++ shared/public_data_access.py
@@ -16,6 +16,14 @@ def create_connection():
     return conn
 
 
+def get_racelist_all(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT slug, url, ended_at, season FROM racelist ORDER BY ended_at DESC")
+    rows = cursor.fetchall()
+    racelist = [RaceRecord(*row)._asdict() for row in rows]
+    return racelist
+
+
 def get_racelist_by_season(conn, season):
     cursor = conn.cursor()
     cursor.execute("""
@@ -25,6 +33,20 @@ def get_racelist_by_season(conn, season):
     """, (season,))
     rows = cursor.fetchall()
     racelist = [RaceRecord(*row, season)._asdict() for row in rows]
+    return racelist
+
+
+def get_racelist_by_player(conn, userid):
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT rl.slug, rl.url, rl.ended_at, rl.season
+        FROM entrants AS e
+        LEFT JOIN racelist AS rl ON e.race_slug = rl.slug
+        WHERE e.user_id=?
+        ORDER BY rl.ended_at DESC
+    """, (userid,))
+    rows = cursor.fetchall()
+    racelist = [RaceRecord(*row)._asdict() for row in rows]
     return racelist
 
 
