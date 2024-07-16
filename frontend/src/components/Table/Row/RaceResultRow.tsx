30a743bf9fe06c49a192ac7f49b25ddfbb66a3cb
Xopar <xoparr@gmail.com>
2024-07-15 19:55:06 -0500

    Frontend formatting for non standard racer participants

--- frontend/src/components/Table/Row/RaceResultRow.tsx
+++ frontend/src/components/Table/Row/RaceResultRow.tsx
@@ -11,30 +11,30 @@ interface RaceResultsData {
   status: string,
   finish_time: string,
   comment: string,
-  delta: number
+  delta: number,
+  ruleset: undefined | null | string
 }
 
 const RaceResultsRow: React.FC<RaceResultsData> = (props) => {
   const isRatingGain: Boolean = props.delta > 0;
   const ordinal = getOrdinal(props.place);
+  const isNonStandard = props.ruleset != null && props.ruleset.toLocaleLowerCase() !== "standard";
 
-  let result: string = "";
-  if (props.status === "dnf") {
-    result = "Forfeit";
-  } else {
-    result = formatDuration(props.finish_time);
-  }
+  const result = props.status === "dnf" ? "Forfeit" : formatDuration(props.finish_time);
 
   return (
     <React.Fragment>
-      <span className={`${styles.placement} ${addPlacementClass(ordinal)}`}>{ordinal}</span>
+      <span className={`${styles.placement} ${addPlacementClass(ordinal)}`}>{isNonStandard ? '' : ordinal}</span>
       <span className={styles.name}>
         <RacetimeName name={props.name} discriminator={props.discriminator} />
       </span>
-      {/* <span className={styles.comment}></span> */}
+      {isNonStandard
+        ? <span className={styles.nonStandardRuleset}>{props.ruleset}</span>
+        : null
+      }
       <span className={styles.time}>{result}</span>
       <span className={`${styles.delta} ${isRatingGain ? styles.positive : styles.negative}`}>
-        {(isRatingGain ? '+' : '') + `${Math.round(props.delta)}`}
+        {isNonStandard ? '' : (isRatingGain ? '+' : '') + `${Math.round(props.delta)}`}
       </span>
     </React.Fragment>
   );  
--- shared/public_data_access.py
+++ shared/public_data_access.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 
 RaceRecord = namedtuple('RaceRecord', ['slug', 'url', 'ended_at', 'season'])
 EntrantRecord = namedtuple('EntrantRecord', [
-    'name', 'discriminator', 'status', 'finish_time', 'place', 'comment', 'delta'
+    'name', 'discriminator', 'status', 'finish_time', 'place', 'comment', 'delta', 'ruleset'
 ])
 
 
@@ -36,11 +36,11 @@ def get_race_entrants(conn, slug):
     cursor.execute(f"""
         SELECT players.name, players.discriminator, entrants.status, 
                entrants.finish_time, entrants.place, entrants.comment,
-               {delta}
+               {delta}, entrants.ruleset
         FROM entrants
         LEFT JOIN players ON entrants.user_id = players.userid
         WHERE entrants.race_slug=?
-        ORDER BY COALESCE(entrants.place, 99999)
+        ORDER BY COALESCE(entrants.finish_time, 'p0DT09H59M59.99S')
     """, (slug,))
     rawentrants = cursor.fetchall()
     entrants = [EntrantRecord(*row)._asdict() for row in rawentrants]
